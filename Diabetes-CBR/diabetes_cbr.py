4077cf5783f1119d51648497f5fcc9bb7e98a7a9
José Júnior <jjunior@ipvc.pt>
2023-01-25 23:53:09 +0000

    Added error handling

--- Diabetes-CBR/diabetes_cbr.py
+++ Diabetes-CBR/diabetes_cbr.py
@@ -1,11 +1,19 @@
 import json
+import importlib
+
+# Read the selected algorithm from config.json
+with open("config.json", "r") as f:
+    config = json.load(f)
+    selected_algorithm = config["selected_algorithm"]
+
+# Import the selected algorithm
+imported_module = importlib.import_module(selected_algorithm)
 
 if __name__ == '__main__':
-    # Read selected algorithm from JSON file
-    with open("config.json", "r") as f:
-        config = json.load(f)
-        selected_algorithm = config["selected_algorithm"]
-
-    # Import and run selected algorithm
-    algorithm = __import__(selected_algorithm)
-    algorithm.run()
+# Error handling
+    try:
+        imported_module.run()
+    except AttributeError as e:
+        print("Error: {}".format(e))
+        print("Ensure that the imported module has a 'run' function.")
+    
