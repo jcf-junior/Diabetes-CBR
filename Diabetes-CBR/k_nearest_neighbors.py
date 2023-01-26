22e2fa3bb89a5ddbdd7b020d63e7d63b038fe8d1
José Júnior <jjunior@ipvc.pt>
2023-01-26 04:53:15 +0000

    update

--- Diabetes-CBR/k_nearest_neighbors.py
+++ Diabetes-CBR/k_nearest_neighbors.py
@@ -1,98 +1,106 @@
-import json
+import importlib
 from math import sqrt
 from Metrics.distance_metrics import *
+import pymongo
 
-def run():
-    ################## Cases ##################
-    # read cases from JSON file
-    with open("cases.json", "r") as f:
-        cases = json.load(f)
-        for case in cases:
-            # fix for KeyError: Time of day
-            case["Time of day"] = 0
+# Connect to the database
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+cases_collection = db["cases"]
+config_collection = db["config"]
+inputs_collection = db["inputs"]
 
-    ################## Metrics ##################
-    # Read selected distance metric from JSON file
-    with open("config.json", "r") as f:
-        config = json.load(f)
-        selected_distance_metric = config["selected_distance_metric"]
-    # Error handler for KeyError
-    try:
-        selected_distance_metric = globals()[selected_distance_metric]
-    except KeyError:
-        print("Invalid distance metric selected.\nExiting...")
-        exit()
+# def run():
+################## Cases ##################
+# read cases from database
+cases = cases_collection.find()
+for case in cases:
+    # fix for KeyError: Time of day
+    case["Time of day"] = 0
 
-    # read inputs from JSON file
-    with open("inputs.json", "r") as f:
-        inputs = json.load(f)
+################## Metrics ##################
+# Read selected distance metric from database
+config = config_collection.find_one({"type": "config"})
+try:
+    selected_distance_metric = config["data"]["selected_distance_metric"]
+except TypeError:
+    print("The variable config is None and therefore it is not subscriptable. \nThis means that the find_one method from the config_collection is not finding any document that matches the query {""type"": ""config""}.\nExiting...")
+    exit()
 
-    # get k from config.json
-    with open("config.json", "r") as f:
-        config = json.load(f)
-        k = int(config["k"])
+# Error handler for KeyError
+try:
+    selected_distance_metric = globals()[selected_distance_metric]
+except KeyError:
+    print("Invalid distance metric selected.\nExiting...")
+    exit()
 
-    # Find k neighbors
-    def find_nearest_neighbors(new_case, k, selected_distance_metric):
-        distances = []
-        for case in cases:
-            distance = selected_distance_metric(new_case, case)
-            distances.append((case, distance))
-        distances.sort(key=lambda x: x[1])
-        return [x[0] for x in distances[:k]]
+# read inputs from database
+inputs = inputs_collection.find_one({"type": "inputs"})
 
-    # Assign inputs to variables
-    preprandial_bg = inputs["Preprandial BG"]
-    iob = inputs["IOB"]
-    bg_target = inputs["BG Target"]
-    cho = inputs["CHO"]
-    patient_weight = inputs["Patient weight"]
-    icr = inputs["ICR"]
-    isf = inputs["ISF"]
-    physical_activity_preprandial_duration = inputs["Physical activity preprandial - Duration"]
-    physical_activity_preprandial_heart_rate = inputs["Physical activity preprandial - Heart rate"]
-    physical_activity_preprandial_intensity = inputs["Physical activity preprandial - Intensity"]
-    physical_activity_postprandial_duration = inputs["Physical activity postprandial - Duration"]
-    physical_activity_postprandial_intensity = inputs["Physical activity postprandial - Intensity"]
-    time_of_day = inputs["Time of day"]
+# get k from config
+k = int(config["data"]["k"])
 
-    # Set the inputs from file
-    new_case = {}
-    new_case["Preprandial BG"] = preprandial_bg
-    new_case["IOB"] = iob
-    new_case["BG Target"] = bg_target
-    new_case["CHO"] = cho
-    new_case["Patient weight"] = patient_weight
-    new_case["ICR"] = icr
-    new_case["ISF"] = isf
-    new_case["Physical activity preprandial - Duration"] = physical_activity_preprandial_duration
-    new_case["Physical activity preprandial - Heart rate"] = physical_activity_preprandial_heart_rate
-    new_case["Physical activity preprandial - Intensity"] = physical_activity_preprandial_intensity
-    new_case["Physical activity postprandial - Duration"] = physical_activity_postprandial_duration
-    new_case["Physical activity postprandial - Intensity"] = physical_activity_postprandial_intensity
-    # Split time of day into hour and minute
-    hour, minute = time_of_day.split(':')
-    hour = int(hour)
-    minute = int(minute)
+# Find k neighbors
+def find_nearest_neighbors(new_case, k, selected_distance_metric):
+    distances = []
+    for case in cases:
+        distance = selected_distance_metric(new_case, case)
+        distances.append((case, distance))
+    distances.sort(key=lambda x: x[1])
+    return [x[0] for x in distances[:k]]
 
-    # Convert time of day to minutes
-    time_in_minutes = hour * 60 + minute
+# Assign inputs to variables
+preprandial_bg = inputs["Preprandial BG"]
+iob = inputs["IOB"]
+bg_target = inputs["BG Target"]
+cho = inputs["CHO"]
+patient_weight = inputs["Patient weight"]
+icr = inputs["ICR"]
+isf = inputs["ISF"]
+physical_activity_preprandial_duration = inputs["Physical activity preprandial - Duration"]
+physical_activity_preprandial_heart_rate = inputs["Physical activity preprandial - Heart rate"]
+physical_activity_preprandial_intensity = inputs["Physical activity preprandial - Intensity"]
+physical_activity_postprandial_duration = inputs["Physical activity postprandial - Duration"]
+physical_activity_postprandial_intensity = inputs["Physical activity postprandial - Intensity"]
+time_of_day = inputs["Time of day"]
 
-    # Assign time of day to new_case
-    new_case["Time of day"] = time_in_minutes
+# Set the inputs from file
+new_case = {}
+new_case["Preprandial BG"] = preprandial_bg
+new_case["IOB"] = iob
+new_case["BG Target"] = bg_target
+new_case["CHO"] = cho
+new_case["Patient weight"] = patient_weight
+new_case["ICR"] = icr
+new_case["ISF"] = isf
+new_case["Physical activity preprandial - Duration"] = physical_activity_preprandial_duration
+new_case["Physical activity preprandial - Heart rate"] = physical_activity_preprandial_heart_rate
+new_case["Physical activity preprandial - Intensity"] = physical_activity_preprandial_intensity
+new_case["Physical activity postprandial - Duration"] = physical_activity_postprandial_duration
+new_case["Physical activity postprandial - Intensity"] = physical_activity_postprandial_intensity
+# Split time of day into hour and minute
+hour, minute = time_of_day.split(':')
+hour = int(hour)
+minute = int(minute)
 
-    # find k nearest neighbors
-    nearest_neighbors = find_nearest_neighbors(
-        new_case, k, selected_distance_metric)
+# Convert time of day to minutes
+time_in_minutes = hour * 60 + minute
 
-    # fix for AttributeError
+# Assign time of day to new_case
+new_case["Time of day"] = time_in_minutes
 
-    # determine recommended insulin bolus
-    recommended_insulin_bolus = 0
-    for neighbor in nearest_neighbors:
-        recommended_insulin_bolus += neighbor["Recommended Insulin Bolus"]
-    recommended_insulin_bolus /= k
+# find k nearest neighbors
+nearest_neighbors = find_nearest_neighbors(
+    new_case, k, selected_distance_metric)
 
-    # print recommended insulin bolus
-    print("Recommended Insulin Bolus: {} U".format(
-        round(recommended_insulin_bolus, 2)))
+# fix for AttributeError
+
+# determine recommended insulin bolus
+recommended_insulin_bolus = 0
+for neighbor in nearest_neighbors:
+    recommended_insulin_bolus += neighbor["Recommended Insulin Bolus"]
+recommended_insulin_bolus /= k
+
+# print recommended insulin bolus
+print("Recommended Insulin Bolus: {} U".format(
+    round(recommended_insulin_bolus, 2)))
