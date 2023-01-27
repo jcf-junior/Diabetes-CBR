3550f7fee5cf5d7005615f84d2d5cc55663b48e4
José Júnior <jjunior@ipvc.pt>
2023-01-27 22:46:25 +0000

    i dont even know

---
+++ Diabetes-CBR/k_nn.py
@@ -0,0 +1,18 @@
+import pymongo
+from math import sqrt
+# from Metrics.distance_metrics import *
+from db_connection import *
+
+# get k from config
+k =int(config["data"]["k"])
+
+# Find k neighbors
+def find_nearest_neighbors(new_case, k, selected_distance_metric):
+    distances = []
+    for case in cases:
+        distance = selected_distance_metric(new_case, case["data"])
+        distances.append((case["data"], distance))
+    distances.sort(key=lambda x: x[1])
+    return [x[0] for x in distances[:k]]
+
+nearest_neighbors = find_nearest_neighbors(new_case["data"], k, selected_distance_metric)
